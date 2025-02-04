4a057110a496d679c98da8d4b852003916a42ed7
Rohan Sunar <rohanksunar15@gmail.com>
2025-02-04 14:43:08 +0530

    Signup User

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -13,11 +16,30 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredEmail = '';
   var _enteredPassword = '';
 
-  void _sumbit(){
-    final _isValid = _form.currentState!.validate();
+  void _sumbit() async {
+    final isValid = _form.currentState!.validate();
 
-    if(_isValid ){
-      _form.currentState!.save();
+    if(!isValid){
+      return;
+    }
+    _form.currentState!.save();
+
+    if(_isLogin  ){
+      //login in
+    } else {
+      try {
+        final userCredentails = await _firebase.createUserWithEmailAndPassword(
+            email: _enteredEmail, password: _enteredPassword);
+        print(userCredentails);
+      } on FirebaseAuthException catch (error) {
+        if (error.code == "email-already-in-use") {
+          //..
+        }
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(error.message ?? 'Authentication failed.'),
+        ));
+      }
     }
   }
 
@@ -85,12 +107,12 @@ class _AuthScreenState extends State<AuthScreen> {
                             height: 12,
                           ),
                           ElevatedButton(
-                              onPressed: _sumbit,
+                              onPressed:  _sumbit,
                               style: ElevatedButton.styleFrom(
                                 backgroundColor:Theme.of(context).colorScheme.primaryContainer,
 
                               ),
-                              child: Text(_isLogin ? 'Signup' : 'Login')),
+                              child: Text(_isLogin ? 'Login' : 'Signup')),
                           TextButton(
                             onPressed: () {
                               setState(() {
@@ -100,10 +122,6 @@ class _AuthScreenState extends State<AuthScreen> {
                             child: Text(_isLogin
                                 ? 'Create an Account'
                                 : 'I already have an account.'),
-
-
-
-
                           ),
                         ],
                       ),
