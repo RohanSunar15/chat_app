3cc9798e4d2ba1c6f2756f31ad2cb1b9801d824f
Rohan Sunar <rohanksunar15@gmail.com>
2025-02-04 14:57:34 +0530

    Login Function is Added

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -24,22 +24,24 @@ class _AuthScreenState extends State<AuthScreen> {
     }
     _form.currentState!.save();
 
-    if(_isLogin  ){
-      //login in
-    } else {
-      try {
-        final userCredentails = await _firebase.createUserWithEmailAndPassword(
+    try {
+      if (_isLogin) {
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
             email: _enteredEmail, password: _enteredPassword);
-        print(userCredentails);
-      } on FirebaseAuthException catch (error) {
-        if (error.code == "email-already-in-use") {
-          //..
-        }
-        ScaffoldMessenger.of(context).clearSnackBars();
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text(error.message ?? 'Authentication failed.'),
-        ));
+        print(userCredentials);
+      } else {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+            email: _enteredEmail, password: _enteredPassword);
+        print(userCredentials);
       }
+    } on FirebaseAuthException catch (error) {
+      if (error.code == "email-already-in-use") {
+        //..
+      }
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(error.message ?? 'Authentication failed.'),
+      ));
     }
   }
 
